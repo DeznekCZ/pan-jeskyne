edef3bcb499bea70331bd7a866da0f90430e26fb
zdeno <zdeno@DESKTOP-20FO24S.lan>
2018-11-11 18:35:53 +0100

    New Models

---
+++ src/main/java/cz/panjeskyne/model/Function.java
@@ -0,0 +1,15 @@
+package cz.panjeskyne.model;
+
+import cz.panjeskyne.service.formula.FunctionTypes;
+
+public class Function {
+
+	public FunctionTypes getType() {
+		return FunctionTypes.FORMULA;
+	}
+
+	public String getFormula() {
+		return "";
+	}
+
+}
---
+++ src/main/java/cz/panjeskyne/model/Table.java
@@ -0,0 +1,18 @@
+package cz.panjeskyne.model;
+
+public class Table {
+
+	public Double getValue(Double[] numbers) {
+		// gets value from table with count of arguments
+		return 0.0;
+	}
+
+	public int getArgsCount() {
+		return 0;
+	}
+
+	public String getCodename() {
+		return "table.name_of_table";
+	}
+
+}
